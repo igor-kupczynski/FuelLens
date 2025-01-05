3696fef600f1fb658cf22d37def5a283d9e723e0
Igor Kupczyński <igor@kupczynski.info>
2025-01-05 12:21:42 +0100

    feat: add testing setup with Vitest and Testing Library

---
+++ .eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: [
+      './tsconfig.json',
+      './tsconfig.node.json',
+      './tsconfig.test.json'
+    ],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx', 'src/test/**/*'],
+      env: {
+        'node': true,
+        'jest': true
+      },
+      globals: {
+        'describe': 'readonly',
+        'it': 'readonly',
+        'expect': 'readonly',
+        'vi': 'readonly'
+      }
+    }
+  ]
+}
---
+++ src/test/setup.ts
@@ -0,0 +1,3 @@
+import 'vitest'
+import type {} from '@testing-library/jest-dom'
+import '@testing-library/jest-dom'
